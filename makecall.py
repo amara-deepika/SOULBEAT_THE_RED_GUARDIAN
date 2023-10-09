aca1f7c61be7d2b08c9eee7050d37146a37ceca6
Amara Deepika <113845145+amara-deepika@users.noreply.github.com>
2023-10-09 17:16:11 +0530

    Update makecall.py

--- makecall.py
+++ makecall.py
@@ -4,14 +4,14 @@ from twilio.rest import Client
 
 # Find your Account SID and Auth Token at twilio.com/console
 # and set the environment variables. See http://twil.io/secure
-account_sid = 'ACfb7f22460b6f2b5bd537927c6b75ad90'
-auth_token = '2f6b2ae403c13b1721c9e220104ff3a9'
+account_sid = 'XXXXXXXXXXXXXXXXXXXX'
+auth_token = 'XXXXXXXXXXXXXXXXXXXX'
 client = Client(account_sid, auth_token)
 a=("b+ donor required" )*5
 call = client.calls.create(
                         twiml='<Response><Say voice="male" language="te"> %s </Say></Response>'%(a),
-                        to='+918072485316',
-                        from_='+13082108219'
+                        to='+91XXXXXXXXX6',
+                        from_='+1XXXXXXX19'
                     )
 
   
