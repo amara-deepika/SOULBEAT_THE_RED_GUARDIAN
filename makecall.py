041723cc790308b0729d0ad012a681bd01501196
Amara Deepika <113845145+amara-deepika@users.noreply.github.com>
2023-10-09 11:02:45 +0530

    Update makecall.py

--- makecall.py
+++ makecall.py
@@ -7,7 +7,7 @@ from twilio.rest import Client
 account_sid = 'ACfb7f22460b6f2b5bd537927c6b75ad90'
 auth_token = '2f6b2ae403c13b1721c9e220104ff3a9'
 client = Client(account_sid, auth_token)
-a=("jagratha "+"dog"+" vastundi" )*5
+a=("b+ donor required" )*5
 call = client.calls.create(
                         twiml='<Response><Say voice="male" language="te"> %s </Say></Response>'%(a),
                         to='+918072485316',
